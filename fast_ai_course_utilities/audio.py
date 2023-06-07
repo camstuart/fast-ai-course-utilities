7b26a97e601ee6824836470663a0717131b37cce
Cam Stuart <cam.asoftware@gmail.com>
2023-06-07 23:37:24 +1000

    return tuple element as numpy

--- fast_ai_course_utilities/audio.py
+++ fast_ai_course_utilities/audio.py
@@ -31,7 +31,7 @@ def librosa_audio_to_mel_spectrogram(sample_rate, audio, n_fft=1024, hop_length=
 
 
 def torch_audio_to_mel_spectrogram(sample_rate: int, signal: torch.Tensor, n_fft: int = 1024, hop_length: int = 256,
-                                   n_mels: int = 40) -> torch.Tensor:
+                                   n_mels: int = 40) -> np.ndarray:
     """Compute a mel spectrogram from a audio data with pytorch"""
     ms = torchaudio.transforms.MelSpectrogram(
         sample_rate=sample_rate,
@@ -40,7 +40,7 @@ def torch_audio_to_mel_spectrogram(sample_rate: int, signal: torch.Tensor, n_fft
         n_mels=n_mels
     )
     mel_spectrogram: torch.Tensor = ms(signal)
-    return mel_spectrogram
+    return mel_spectrogram[0].numpy()
 
 
 def load_torch_audio_file(path: str) -> Tuple[torch.Tensor, int]:
